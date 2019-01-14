abade2b3105538499fca682cffb00e6a11012a10
Ermakoy <own3d.rus@gmail.com>
2019-01-14 07:59:45 +0300

    add hint to addition

--- AdditionlessAddition.js
+++ AdditionlessAddition.js
@@ -1,3 +1,4 @@
 // http://www.codewars.com/kata/additionless-addition
+// Hint: https://prismoskills.appspot.com/lessons/Bitwise_Operators/Sum_using_only_bitwise_ops.jsp
 
 const add = (x, y) => !x ? y : add((x & y) << 1, x ^ y);
